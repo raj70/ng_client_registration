a07c715d9a7bec643397f6c4675118dad9d2a147
rajenshrestha@hotmail.com <rajenshrestha@hotmail.com>
2020-06-04 20:00:05 +1000

    add migrations

---
+++ Src/Rs.App.Core.Ng.ClientRegistration.Domain/Migrations/20200604095538_InitialDb.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rs.App.Core.ClientRegistration.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    Line1 = table.Column<string>(maxLength: 250, nullable: false),
+                    Line2 = table.Column<string>(maxLength: 250, nullable: true),
+                    Line3 = table.Column<string>(maxLength: 250, nullable: true),
+                    CompareConcatenated = table.Column<string>(maxLength: 1501, nullable: true),
+                    Suburb = table.Column<string>(maxLength: 250, nullable: false),
+                    Postcode = table.Column<string>(maxLength: 250, nullable: false),
+                    Country = table.Column<string>(maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    FirstName = table.Column<string>(maxLength: 250, nullable: false),
+                    LastName = table.Column<string>(maxLength: 250, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    PhoneNumber = table.Column<string>(maxLength: 30, nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    AddressId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Credentials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    CreatedDateTime = table.Column<DateTime>(nullable: false),
+                    Username = table.Column<string>(maxLength: 250, nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    ClientId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Credentials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Credentials_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_AddressId",
+                table: "Clients",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Credentials_ClientId",
+                table: "Credentials",
+                column: "ClientId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Credentials");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
